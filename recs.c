64973a4ea24dccf87d3d6ac03fc4585c5bdbbd40
tchieze <allekmott@gmail.com>
2016-03-10 19:54:08 -0800

    Initial commit

---
+++ recs.c
@@ -0,0 +1,21 @@
+/* recs.c
+ * RECord Search
+ * Tool to simplify record hunts;
+ * Organizes artists into alphabetized list,
+ * allows for numerous traversal methods (in real-time).
+ *
+ * Created: 10 March 2016
+ */
+
+#include <stdio.h>
+
+#define RECS_VERSION "0.0.1"
+
+void version() {
+	printf("recs v%s\n", RECS_VERSION);
+}
+
+int main(int argc, char const *argv[]) {
+	version();
+	return 0;
+}
